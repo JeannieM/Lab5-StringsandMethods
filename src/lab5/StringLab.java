8a5792252b31cc97f895b0d7e68bb575dd24f93c
JeannieM <bean.mccarthy@gmail.com>
2020-01-24 00:24:00 -0500

    first commit

---
+++ src/lab5/StringLab.java
@@ -0,0 +1,81 @@
+package lab5;
+/*
+ * @author jeanniemccarthy
+ */
+
+import java.util.Scanner;
+
+public class StringLab {
+	public static void main(String[] args) {
+		
+		Scanner scnr = new Scanner(System.in);
+		StringMethods stringMethodsObject = new StringMethods();
+		String word;
+		String phrase;
+		int waldoAt;
+		String string1, string2, inOrder;
+		String reverseThisWord;
+		StringBuilder wordReversed;
+		String word1, word2, reOrder;
+		String afterMath;
+		String letterized;
+		String camelizedPhrase;
+		
+		System.out.println("Enter a word in all caps: ");
+		word = "blAZes"; //scnr.nextLine();
+		
+		word = stringMethodsObject.capitalize(word);
+		System.out.println(word);
+		
+		System.out.println("Enter a phrase with Waldo hiding somewhere:");
+		phrase = "Striped Waldo!"; //scnr.nextLine();
+		waldoAt = stringMethodsObject.wheresWaldo(phrase);
+		System.out.println(waldoAt);
+		
+		System.out.println("Enter two words:");
+		string1 = "tree"; //scnr.next();
+		string2 = "pine"; //scnr.next();
+		inOrder = stringMethodsObject.firstThingsFirst(string1, string2);
+		System.out.println(inOrder);
+		
+		System.out.println("Enter a word for reversal:");
+		reverseThisWord = "happy"; //scnr.next();
+		//String [] wordArr = reverseThisWord.split("");
+		wordReversed = stringMethodsObject.reverse(reverseThisWord);
+		System.out.println(wordReversed);
+		
+		System.out.println("Enter two words:");
+		word1 = "bean"; //scnr.next();
+		word2 = "pea"; //scnr.next();
+		reOrder = stringMethodsObject.soLong(word1, word2);
+		System.out.println(reOrder);
+		
+		System.out.println("Enter a phrase that may or may not have the word \"math\" somewhere in there:");
+		phrase = "nobody thinks math is cool"; //scnr.nextLine();
+		afterMath = stringMethodsObject.afterMath(phrase);
+		System.out.println(afterMath);
+		
+		System.out.println("Enter a word to letterize:");
+		word = "java"; //scnr.nextLine();
+		letterized = stringMethodsObject.letterize(word);
+		System.out.println(letterized);
+		
+		System.out.println("Enter a phrase:");
+		phrase = scnr.nextLine();
+		//System.out.println(camelizedPhrase);
+		
+		String firstWord = phrase.substring(0, phrase.indexOf(" "));//you could take this first word substring and use it 
+		//later to replace (0, firstWord.length())
+		String[] phraseSplit = phrase.split(" ");
+		String wordCamel = "";
+		for (String wordC : phraseSplit) {
+			wordCamel = wordCamel.toUpperCase();
+			wordCamel = wordCamel.substring(0,2) + word.substring(1).toLowerCase();	
+		}
+		System.out.println(wordCamel);
+		
+		//String wordLetter1 = phrase.replace(phrase.charAt(phrase.indexOf(" ")+1), Character.toUpperCase(phrase.charAt(phrase.indexOf(" ")+1))) ;
+		//System.out.println(wordLetter1);
+	}
+
+}
---
+++ src/lab5/StringMethods.java
@@ -0,0 +1,73 @@
+package lab5;
+/*
+ * @author jeanniemccarthy
+ */
+public class StringMethods {
+	StringBuilder strBuild = new StringBuilder();
+	
+	public String capitalize(String word) {
+		word = word.toUpperCase().charAt(0) + word.substring(1).toLowerCase();
+		return word;
+	}
+	public int wheresWaldo(String phrase) {
+		int waldo = phrase.indexOf("Waldo");
+		return waldo;
+	}
+	public String firstThingsFirst(String string1, String string2) {
+		String inOrder;
+		if (string1.compareTo(string2) <= 0) {
+			 inOrder = string1 + " " + string2;
+		}
+		else {
+			inOrder = string2 + " " + string1;
+		}
+		return inOrder;
+	}
+	
+	public StringBuilder reverse(String wordOriginal) {
+		strBuild.append(wordOriginal);
+		strBuild.reverse();
+		return strBuild;
+		}
+	
+	public String soLong(String word1, String word2) {
+		String reOrder;
+		if (word1.length() > word2.length()) {
+			 reOrder = word1;
+		}
+		else if (word2.length() > word1.length()){
+			 reOrder = word2;
+		}
+		else {
+			reOrder = word2 + " " + word1;
+		}
+		return reOrder;
+	}
+	
+	public String afterMath(String phrase) {
+		String afterMath;
+		if (phrase.indexOf("math") != -1) {
+			afterMath = phrase.substring(phrase.indexOf("math"));
+		}
+		else {
+			afterMath = "dud";
+		}
+		return afterMath;
+	}
+	
+	public String letterize(String word) {
+		String [] arr = word.split("");
+		String verticalWord = "";
+		for (String letter : arr) {
+			verticalWord += letter +"\n";
+		}
+		return verticalWord;
+	}
+	
+	public String camelized(String phrase) {
+		char wordLetter1;
+		String camelizedPhrase = "";
+		wordLetter1 = phrase.charAt(phrase.indexOf(" ")+1);
+		return camelizedPhrase;
+	}
+}
